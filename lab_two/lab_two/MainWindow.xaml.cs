08539f6b8d36cedfb14ba2ae871b87491cfa886e
Max <meitstudying@gmail.com>
2025-03-19 21:20:31 +0200

    Task 4

--- lab_two/lab_two/MainWindow.xaml.cs
+++ lab_two/lab_two/MainWindow.xaml.cs
@@ -11,14 +11,76 @@ using System.Windows.Shapes;
 
 namespace lab_two
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static RoutedCommand ClearCommand = new RoutedCommand();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Register command bindings
+            CommandBindings.Add(new CommandBinding(
+                ApplicationCommands.Save,
+                SaveCommand_Execute,
+                SaveCommand_CanExecute));
+
+            CommandBindings.Add(new CommandBinding(
+                ApplicationCommands.Open,
+                OpenCommand_Execute,
+                OpenCommand_CanExecute));
+
+            CommandBindings.Add(new CommandBinding(
+                ClearCommand,
+                ClearCommand_Execute,
+                ClearCommand_CanExecute));
+
+            CommandBindings.Add(new CommandBinding(
+                ApplicationCommands.Paste, 
+                Paste_Execute, 
+                Paste_CanExecute));
+        }
+
+        private void SaveCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !string.IsNullOrEmpty(textBox.Text);
+        }
+
+        private void SaveCommand_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            // Implement save functionality
+            MessageBox.Show("Text saved!");
+        }
+
+        private void OpenCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OpenCommand_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            // Implement open functionality
+            MessageBox.Show("Open file dialog!");
+        }
+
+        private void ClearCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !string.IsNullOrEmpty(textBox.Text);
+        }
+
+        private void ClearCommand_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            textBox.Clear();
+        }
+
+        private void Paste_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = Clipboard.ContainsText();
+        }
+
+        private void Paste_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            textBox.Paste();
         }
     }
 }
